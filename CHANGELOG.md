cfada44775730a8778b7e8f54ce1700129e541d7
Patrick Scherer <patrick.scherer@shopgate.com>
2019-09-04 15:39:46 +0200

    CCP-1606: configurable portal positions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/) and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.0.2] - 2019-09-03
+### Added
+- Configurable portal positions
+
 ## [1.0.1] - 2019-08-19
 ### Fixed
 - Set PWA dependency to 6.7
@@ -12,4 +16,3 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/) and this p
 ### Added
 - First implementation
 - Added configurable widget to display Recommended product slider teaser and contains a button link to recommendations page.
-
--- frontend/portals/Recommendations/index.jsx
+++ frontend/portals/Recommendations/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useCurrentProduct, useRoute } from '@shopgate/engage/core';
 import { CART_PATH } from '@shopgate/engage/cart';
 import ProductSlider from '../../components/ProductSlider';
 import { RECOMMENDATION_TYPE_CART, RECOMMENDATION_TYPE_PRODUCT } from '../../constants';
+import { productPage, cartPage } from '../../config';
 
 /**
  * Product slider on PDP page.
  * @returns {JSX}
  */
-const ProductTaxDisclaimerAfter = () => {
+const Recommendations = ({ name }) => {
   const { pathname } = useRoute();
 
-  if (pathname === CART_PATH) {
+  // Cart Portal position is different than configured.
+  if (pathname === CART_PATH && name === cartPage.position) {
     return (
       <ProductSlider type={RECOMMENDATION_TYPE_CART} />
     );
@@ -23,9 +26,18 @@ const ProductTaxDisclaimerAfter = () => {
     return null;
   }
 
+  // PDP Portal position is different than configured.
+  if (name !== productPage.position) {
+    return null;
+  }
+
   return (
     <ProductSlider type={RECOMMENDATION_TYPE_PRODUCT} id={productId} />
   );
 };
 
-export default ProductTaxDisclaimerAfter;
+Recommendations.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default Recommendations;
